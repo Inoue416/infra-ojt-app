909a318b27c483ac5d3f4e99cf3c04b3c8572f72
井上 優也 <usr0105394@gmo.local>
2024-06-07 00:05:31 +0900

    簡素なchatベースのUI作成、streamingでの表示実装、ファイル入力実装 #2 #1

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import gradio as gr
+import env
+from chatbot import chat, llmbot
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
+# Chatbot app with multimodal input (text, json, csv). Plus shows support for streaming text.
+# ASK: できればこのブロックも別ファイルに分けたいがアプリ規模的にもメインになるため分ける必要はないかも
+with gr.Blocks() as app:
+    chatbot = gr.Chatbot(
+        [],
+        elem_id="chatbot",
+        bubble_full_width=False
+    )
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
-)
+    chat_input = gr.MultimodalTextbox(
+        interactive=True, 
+        file_types=[".json", ".txt", ".csv"], 
+        placeholder="Enter message or upload file...",
+        show_label=False
+    )
 
-demo.launch(server_port=7861, server_name="127.0.0.1")
+    chat_msg = chat_input.submit(chat.addUserInput, [chatbot, chat_input], [chatbot, chat_input])
+    # TODO: バリデーションを入れる
+    bot_msg = chat_msg.then(
+        llmbot.callBot,  # LLMのpredictを呼ぶ
+        chatbot, chatbot, api_name="bot_response")
+    bot_msg.then(lambda: gr.MultimodalTextbox(interactive=True), None, [chat_input])
+
+app.queue()
+app.launch(server_port=env.server_port, server_name=env.server_name)
